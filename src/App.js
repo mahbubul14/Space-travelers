1980a9f3e93b74cd756d688ab2fdac9249d86e54
mahbubul14 <mahbub21alam2017@gmail.com>
2021-11-18 07:24:22 +0600

    Update navlink

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Rockets from './component/Rockets';
 import MyProfile from './component/MyProfile';
+import Missions from './component/Missions';
 import Nav from './component/Nav';
 import { loadRocketsData } from './redux/rockets/rockets';
 import './App.css';
@@ -18,6 +19,9 @@ const App = () => {
       <Nav />
       <Routes>
         <Route exact path="/" element={<Rockets />} />
+      </Routes>
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
       </Routes>
       <Routes>
         <Route path="/my-profile" element={<MyProfile />} />

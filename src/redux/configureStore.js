9fb698e4ea32ec3afd1c9d09b384882a1b64faa6
mahbubul14 <mahbub21alam2017@gmail.com>
2021-11-18 07:17:32 +0600

    Delete missions  things

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { rocketsReducer } from './rockets/rockets';
-import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
   rocketsReducer,
-  missionsReducer,
 });
 const middleware = [logger, thunk];
 

901af1451f41d403aebd3481c92bf95ac29f029d
mahbubul14 <mahbub21alam2017@gmail.com>
2021-11-18 07:19:46 +0600

    Update app js file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Rockets from './component/Rockets';
-import Missions from './component/Missions';
 import MyProfile from './component/MyProfile';
 import Nav from './component/Nav';
 import './App.css';
-import store from './redux/configureStore';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<Rockets />} />
-        </Routes>
-        <Routes>
-          <Route path="/missions" element={<Missions />} />
-        </Routes>
-        <Routes>
-          <Route path="/my-profile" element={<MyProfile />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+      </Routes>
+      <Routes>
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
